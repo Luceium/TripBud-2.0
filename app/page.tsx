9b29093c742232ef306ada704abe94aa9063156c
Mau <79354255+Luceium@users.noreply.github.com>
2023-09-25 01:57:46 -0700

    makes first page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
-import Image from 'next/image'
+import LogInSignOut from "./components/LogInSignOut";
+
 
 export default function Home() {
+
   return (
     <main>
-      <div className="container">
-        <h1 className="home-title">TripBud</h1>
-        <h3 className="home-subtitle">budget trip buddy</h3>
-        <div className="enter-btn-div" id="enter-btn-div"></div>
+      <div className="flex flex-col">
+        <h1 className="home-title text-center text-xl py-20 mt-20">TripBud</h1>
+        <h3 className="home-subtitle text-center text-sm py-5">your budget trip planner</h3>
+        <LogInSignOut />
       </div>
-      <div className="footer">
+      <div className="absolute -bottom-0 text-center text-xs w-full">
         <p>© 2022 SPS Team 36</p>
       </div>
     </main>
